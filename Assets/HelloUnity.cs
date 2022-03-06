9ca7a094d64056f97e31ab750591dd78e6dfaaaa
yoon sang seok (kyle) <amamov@kakao.com>
2022-03-06 21:46:33 +0900

    feat : c# var

--- Assets/HelloUnity.cs
+++ Assets/HelloUnity.cs
@@ -7,9 +7,16 @@ public class HelloUnity : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!!");
+        //* C# Var
         int age = 25;
+        float height = 183.3829f; // 32bit (소숫점 아래 7자리까지 정확)
+        double pi = 3.14159265359; // 64bit (소숫점 아래 15자리까지 정확)
+        bool isSexy = true;
+        char grade = 'A'; // character 한 문자
+        string title = "Hello World!!";
         Debug.Log(age);
+        Debug.Log(pi);
+        Debug.Log(title + " bro!" + pi);
     }
 
     // Update is called once per frame
