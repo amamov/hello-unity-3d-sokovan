c5ad36917f64208389a440a04eac40ab90b85065
yoon sang seok (kyle) <amamov@kakao.com>
2022-03-06 23:44:20 +0900

    feat : c# function

--- Assets/HelloUnity.cs
+++ Assets/HelloUnity.cs
@@ -1,30 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HelloUnity : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         //* C# Var
-        int age = 25;
-        float height = 183.3829f; // 32bit (소숫점 아래 7자리까지 정확)
-        double pi = 3.14159265359; // 64bit (소숫점 아래 15자리까지 정확)
-        bool isSexy = true;
-        char grade = 'A'; // character 한 문자
-        string title = "Hello World!!";
+        var age = 25;
+        var height = 183.3829f; // 32bit (소숫점 아래 7자리까지 정확)
+        var pi = 3.14159265359; // 64bit (소숫점 아래 15자리까지 정확)
+        var isSexy = true;
+        var grade = 'A'; // character 한 문자
+        var title = "Hello World!!";
         Debug.Log(age);
         Debug.Log(pi);
         Debug.Log(title + " bro!" + pi);
         var name = "yoon sang seok"; // auto typing
-        
 
+        //* C# Function
+        var sizeOfCircle = 30f;
+        var radius = GetRadius(sizeOfCircle);
+        Debug.Log(radius);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
 
+    }
+
+    private float GetRadius(float size)
+    {
+        var pi = 3.14f;
+        var radius = Mathf.Sqrt(size / pi);
+        return radius;
     }
 }
